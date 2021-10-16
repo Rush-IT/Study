3d298c1803e891e43d68e46ec7dfc46c7e067314
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2021-10-16 15:16:02 +0500

    Update LB_1.1.cs

--- LB_1.1.cs
+++ LB_1.1.cs
@@ -11,28 +11,28 @@ namespace ЛБ_1._1
             bool flag;
             do
             {
-                Console.WriteLine("Введите значение n: ");
+                Console.Write("Введите значение n: ");
                 flag = int.TryParse(Console.ReadLine(), out n);
                 if (!flag) Console.WriteLine("Введено не верное значение!\nПопробуйте снова");
             } while (!flag);
             do
             {
-                Console.WriteLine("Введите значение m: ");
+                Console.Write("Введите значение m: ");
                 flag = int.TryParse(Console.ReadLine(), out m);
                 if (!flag) Console.WriteLine("Введено не верное значение!\nПопробуйте снова");
             } while (!flag);
             Console.WriteLine("n = " + (int)n + "\nm = " + (int)m);
             //(1)\\
             int R1 = n++ * --m;
-            Console.WriteLine("R1 = " + (int)R1);
+            Console.WriteLine("n++ * --m = " + (int)R1);
             Console.WriteLine("n = " + (int)n + "\nm = " + (int)m);
             //(2)\\
             bool R2 = n-- < m++;
-            Console.WriteLine("R2 = " + (bool)R2);
+            Console.WriteLine("n-- < m++ = " + (bool)R2);
             Console.WriteLine("n = " + (int)n + "\nm = " + (int)m);
             //(3)\\
             bool R3 = --n > --m;
-            Console.WriteLine("R3 = " + (bool)R3);
+            Console.WriteLine("--n > --m = " + (bool)R3);
             Console.WriteLine("n = " + (int)n + "\nm = " + (int)m);
             //(4)\\
             double x;
@@ -40,11 +40,15 @@ namespace ЛБ_1._1
             {
                 Console.WriteLine("Введите значение x: ");
                 flag = double.TryParse(Console.ReadLine(), out x);
-                if (x == 1 || x == -1) Console.WriteLine("Ошибка!\nПри данном занчении произойдет деление на ноль!\nПопробуйте снова");
                 if (!flag) Console.WriteLine("Введено не верное значение!\nПопробуйте снова");
+                if (x == 1 || x == -1)
+                {
+                    Console.WriteLine("Ошибка!\nПри данном занчении произойдет деление на ноль!\nПопробуйте снова");
+                    flag = false;
+                }
             } while (!flag);
             double value = Math.Pow(Math.Abs(x + 1), 0.25) + Math.Pow(x, -2);
-            Console.WriteLine("R4 = " + (double)value);
+            Console.WriteLine("(|x+1|)^(1/4)+(1/x^2) = " + value);
         }
     }
 }
