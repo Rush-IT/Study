7fe687621513ea4cb1a6ba27631253822f2d7673
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2021-12-26 00:20:40 +0500

    Add files via upload

---
+++ LB_OOP/LB_OOP_11/Birds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LB_OOP_11
+{
+    public class Birds : Animals
+    {
+        string name = "";
+        int scope = 0;
+        public Animals BaseAnimals
+        {
+            get => new Animals(type, weight);
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int Scope
+        {
+            get => scope;
+            set => scope = value;
+        }
+
+        public Birds() : base()
+        {
+            name = "";
+        }
+        public Birds(string n, string t, int s, int w) : base(t, w)
+        {
+            name = n;
+            scope = s;
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Вид: {name}");
+            Console.WriteLine($"Тип: {Type}");
+            Console.WriteLine($"Размах крыла: {scope} см");
+            Console.WriteLine($"Вес: {Weight}кг");
+        }
+    }
+}
---
+++ LB_OOP/LB_OOP_11/SortByWeight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LB_OOP_11
+{
+    class SortByWeight 
+    {
+        public int Compare(Animals x, Animals y)
+        {
+            if (x.Weight > y.Weight) return 1;
+            if (x.Weight < y.Weight) return -1;
+            return 0;
+        }
+    }
+}
